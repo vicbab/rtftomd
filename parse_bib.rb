31e5f94e95d31f750fdc4d0e92beb7bb50de0739
Victor Babin <victor.babin@umontreal.ca>
2024-02-27 16:36:32 -0500

    anystyle intergration

--- parse_bib.rb
+++ parse_bib.rb
@@ -1,9 +1,22 @@
 require 'anystyle'
 
-refs = AnyStyle.find ARGV[0]
+file = File.open(ARGV[0], "r")
 
-puts refs.to_s
+#Make a copy
+file_copy = File.open("#{ARGV[0]}.txt", "w")
+
+content = file.read.encode('UTF-8', :invalid => :replace, :undef => :replace, :replace => '')
+content = content.gsub("*", "")
+content = content.gsub(">", "")
+
+file_copy.write(content)
+file_copy.close
+file.close
+
+refs = AnyStyle.find file_copy.path
 
 bib = AnyStyle.parse refs.to_s.split("\","), format: 'bibtex'
+
+puts "Writing to #{ARGV[0]}.bib"
 
 File.write("#{ARGV[0]}.bib", bib.to_s)
